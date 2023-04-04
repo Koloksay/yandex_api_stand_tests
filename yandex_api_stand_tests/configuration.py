471e467541500d014e40eb3ea5498330517a01d8
Katrechko_A_S <crossworld@yandex.ru>
2023-04-04 13:37:37 +0300

    My first commit

---
+++ yandex_api_stand_tests/configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://1e554837-10db-432d-95c3-cbe309144f94.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
